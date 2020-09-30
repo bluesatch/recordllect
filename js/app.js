7540445c81df98973a19e7fbca354602ef103610
H Brown <hebrown12@gmail.com>
2020-09-30 14:52:00 -0500

    completed user-profile page; renamed file to user-profile.html

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@ $(function() {
     $('.burger-slice').click(function() {
         $('#burger-list').toggleClass('display-off', 'display-on')
     })
+
+    $('.see-more').click(function() {
+        $('.album-card').css('display', 'block')
+    })
 });
